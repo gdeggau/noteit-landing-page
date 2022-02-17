d7e90ee31528485fe985b775416601fc19f60ea8
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-02-17 10:09:55 -0300

    feat: add Pencil icon

---
+++ src/components/Icons/Pencil.tsx
@@ -0,0 +1,31 @@
+import { Icon, IconProps } from '@chakra-ui/icons'
+
+export const PencilIcon = (props: IconProps) => {
+  return (
+    <Icon width="165" height="30" viewBox="0 0 165 30" fill="none" {...props}>
+      <path
+        d="M121.207 -1.90735e-06V30L163.83 16.5789V14.2105L121.207 -1.90735e-06Z"
+        fill="#B1E2CB"
+      />
+      <path
+        d="M121.207 -1.90735e-06V30L-23.0549 30V-1.90735e-06L121.207 -1.90735e-06Z"
+        fill="#44484B"
+      />
+      <path
+        d="M126.625 2.32836V27.765L128.084 27.3075V2.81508L126.625 2.32836Z"
+        stroke="#717171"
+      />
+      <path
+        d="M130.724 3.70348V26.4855L132.183 26.0357V4.18957L130.724 3.70348Z"
+        stroke="#717171"
+      />
+      <rect
+        width="17.3684"
+        height="119.672"
+        rx="7.52631"
+        transform="matrix(0 -1 -1 0 108.093 23.6842)"
+        fill="#B1E2CB"
+      />
+    </Icon>
+  )
+}

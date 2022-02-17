0f5bb31e427b8ea005731a534986bd825ef43419
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-02-16 21:43:56 -0300

    refactor: remove overflow hidden from AppWrapper

--- src/layouts/AppWrapper.tsx
+++ src/layouts/AppWrapper.tsx
@@ -1,13 +1,5 @@
 import { Flex, FlexProps } from '@chakra-ui/react'
 
 export const AppWrapper = (props: FlexProps) => {
-  return (
-    <Flex
-      flexDirection="column"
-      overflow="hidden"
-      height="100vh"
-      width="100vw"
-      {...props}
-    />
-  )
+  return <Flex flexDirection="column" height="100vh" width="100vw" {...props} />
 }

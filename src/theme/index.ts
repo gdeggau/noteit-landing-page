e93b3fe6c206bf91301e3e627fe590f33d309842
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-02-16 21:42:50 -0300

    feat: add some textStyles

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -18,33 +18,53 @@ const theme = extendTheme({
       100: '#DEDEDE',
       50: '#F2F2F6',
     },
+
+    green: {
+      50: '#B1E2CB',
+    },
   },
 
   fonts: {
     body: 'Poppins, sans-serif',
-    heading: 'Poppins, sans-serif',
+    // heading: 'Poppins, sans-serif',
   },
 
   textStyles: {
     'LargeTitle/Regular': {
       fontSize: ['14px', '27px'],
       fontWeight: '400',
-      lineHeight: '100%',
+      // lineHeight: '100%',
     },
     'LargeTitle/Semibold': {
       fontSize: ['14px', '27px'],
       fontWeight: '600',
-      lineHeight: '100%',
+      // lineHeight: '100%',
     },
     'Title1/Regular': {
       fontSize: '21px',
       fontWeight: '400',
-      lineHeight: '31px',
+      // lineHeight: '31px',
     },
     'Title1/Semibold': {
       fontSize: '21px',
       fontWeight: '600',
-      lineHeight: '31px',
+      // lineHeight: '31px',
+    },
+    'Title/Regular': {
+      fontSize: ['13px', '17px'],
+      fontWeight: '400',
+      // lineHeight: '25px',
+    },
+    'Title/Semibold': {
+      fontSize: ['13px', '17px'],
+      fontWeight: '600',
+      // lineHeight: '25px',
+    },
+    'Other/Gamja': {
+      fontFamily: 'Gamja Flower, cursive',
+      fontSize: '24px',
+      fontWeight: '400',
+      color: 'green.50',
     },
   },
 })

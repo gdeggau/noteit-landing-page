40facf6e88920c0ea6e97157ab33fe595b40756a
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-02-17 10:11:10 -0300

    feat: makes Footer component responsiveness

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,31 +1,86 @@
-import { HStack, Link, Text, VStack } from '@chakra-ui/react'
+import { HStack, IconProps, Link, Text, VStack } from '@chakra-ui/react'
 
+import { PencilIcon, PencilScribbleIcon } from '@components'
 import { routes } from '@constants'
+
+const Pencil = (props: IconProps) => {
+  return (
+    <PencilIcon
+      display={{
+        base: 'none',
+        md: 'flex',
+      }}
+      width={{
+        base: '100px',
+        lg: '200px',
+      }}
+      {...props}
+    />
+  )
+}
+const PencilScribble = (props: IconProps) => {
+  return (
+    <PencilScribbleIcon
+      display={{
+        base: 'none',
+        md: 'flex',
+        lg: 'none',
+        xl: 'flex',
+      }}
+      width={{
+        base: '100px',
+        lg: '180px',
+      }}
+      {...props}
+    />
+  )
+}
 
 export const Footer = () => {
   return (
     <VStack bgColor="gray.500" spacing={0}>
       <HStack
+        justify="space-between"
         width="100%"
-        spacing={4}
-        justify="center"
+        align="center"
         pb={{
           base: 14,
+          lg: 36,
         }}
         pt={{
           base: 5,
+          lg: 8,
         }}
       >
-        <Text textStyle="Other/Gamja">Draw</Text>
-        <Text textStyle="Other/Gamja">Share</Text>
-        <Text textStyle="Other/Gamja">Have fun</Text>
+        <HStack>
+          <Pencil />
+          <PencilScribble />
+        </HStack>
+        <HStack
+          spacing={{
+            base: 4,
+            lg: 12,
+          }}
+          justify="center"
+        >
+          <Text textStyle="Other/Gamja">Draw</Text>
+          <Text textStyle="Other/Gamja">Share</Text>
+          <Text textStyle="Other/Gamja">Have fun</Text>
+        </HStack>
+        <HStack>
+          <PencilScribble transform={'scaleX(-1)'} />
+          <Pencil transform={'rotate(180deg)'} />
+        </HStack>
       </HStack>
       <HStack
         width="100%"
         borderTopLeftRadius="50px"
         spacing={8}
         justify="center"
-        py={8}
+        py={{
+          base: 8,
+          lg: 12,
+        }}
         bgColor="white"
       >
         <Link href={routes.privacyPolicy.path} textStyle="Title/Semibold">

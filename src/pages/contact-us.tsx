dc13488eca1a2d188aa991ffb0ee3418f185610e
gabrieldeggau <gabriel.deggau@premiersoft.net>
2022-02-21 18:34:30 -0300

    feat: save form data in a state

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'
+
 import {
   Button,
   FormControl,
@@ -12,55 +14,79 @@ import { SocialNetworks } from '@components'
 import { DefaultPage } from '@layouts'
 
 const ContactUs = () => {
+  const [formData, setFormData] = useState({})
   const { t } = useTranslation()
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
   return (
-    <DefaultPage
-      title={t('contact-us.header.title')}
-      subtitle={t('contact-us.header.subtitle')}
-      mainContent={
-        <Stack
-          as="form"
-          spacing={{ base: 5, lg: 7 }}
-          align="center"
-          textStyle="Body/Regular"
-        >
+    <>
+      <DefaultPage
+        title={t('contact-us.header.title')}
+        subtitle={t('contact-us.header.subtitle')}
+        mainContent={
           <Stack
-            direction={{
-              base: 'column',
-              md: 'row',
-            }}
-            spacing={{
-              base: 5,
-            }}
-            width="100%"
+            as="form"
+            spacing={{ base: 5, lg: 7 }}
+            align="center"
+            textStyle="Body/Regular"
+            onSubmit={handleSubmit}
           >
+            <Stack
+              direction={{
+                base: 'column',
+                md: 'row',
+              }}
+              spacing={{
+                base: 5,
+              }}
+              width="100%"
+            >
+              <FormControl>
+                <FormLabel htmlFor="name">
+                  {t('contact-us.form.label.name')}
+                </FormLabel>
+                <Input id="name" name="name" onChange={handleChange} />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="email">
+                  {t('contact-us.form.label.email')}
+                </FormLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                />
+              </FormControl>
+            </Stack>
             <FormControl>
-              <FormLabel htmlFor="name">
-                {t('contact-us.form.label.name')}
+              <FormLabel htmlFor="message">
+                {t('contact-us.form.label.message')}
               </FormLabel>
-              <Input id="name" />
+              <Textarea id="message" name="message" onChange={handleChange} />
             </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="email">
-                {t('contact-us.form.label.email')}
-              </FormLabel>
-              <Input id="email" type="email" />
-            </FormControl>
+            <Button maxW={80} isFullWidth type="submit">
+              {t('contact-us.form.button.send')}
+            </Button>
           </Stack>
-          <FormControl>
-            <FormLabel htmlFor="message">
-              {t('contact-us.form.label.message')}
-            </FormLabel>
-            <Textarea id="message" />
-          </FormControl>
-          <Button maxW={80} isFullWidth type="submit">
-            {t('contact-us.form.button.send')}
-          </Button>
-        </Stack>
-      }
-    >
-      <SocialNetworks direction="row" />
-    </DefaultPage>
+        }
+      >
+        <SocialNetworks direction="row" />
+      </DefaultPage>
+    </>
   )
 }
 
